47aa7c1b6738ee7a35e534c642d39a9565d10f83
Marco <25ci0119@jec.ac.jp>
2026-09-09 15:00:04 +0900

    月追加、フェードアウト修正
    ・フェードアウト時、変なウィジェット...のような何かが出てたので修正
    ・つかめない時の効果音の音量を上げた
    ・LV_End2に月のモデルを追加

--- Source/TeamClimbVR/IntroPlayer.cpp
+++ Source/TeamClimbVR/IntroPlayer.cpp
@@ -1,27 +1,11 @@
-#include "IntroPlayer.h"
+ï»¿#include "IntroPlayer.h"
 #include "Kismet/GameplayStatics.h"
 #include "Camera/PlayerCameraManager.h"
 #include "TimerManager.h"
-#include "Blueprint/UserWidget.h"
 
 AIntroPlayer::AIntroPlayer()
 {
-    PrimaryActorTick.bCanEverTick = true;
-
-    LogoWidget = CreateDefaultSubobject<UWidgetComponent>(TEXT("LogoWidget"));
-    LogoWidget->SetupAttachment(RootComponent);
-
-    LogoWidget->SetWidgetSpace(EWidgetSpace::World);
-    LogoWidget->SetDrawSize(FVector2D(1024, 512));
-    LogoWidget->SetPivot(FVector2D(0.5f, 0.5f));
-    LogoWidget->SetCollisionEnabled(ECollisionEnabled::NoCollision);
-
-    LogoWidget->SetTickWhenOffscreen(true);
-    LogoWidget->SetTwoSided(true);
-    LogoWidget->SetBlendMode(EWidgetBlendMode::Transparent);
-    LogoWidget->SetBackgroundColor(FLinearColor::Transparent);
-
-    LogoWidget->SetTranslucentSortPriority(9999);
+    PrimaryActorTick.bCanEverTick = false;
 }
 
 void AIntroPlayer::BeginPlay()
@@ -38,16 +22,6 @@ void AIntroPlayer::BeginPlay()
         true
     );
 
-    FVector CamLoc;
-    FRotator CamRot;
-    PC->GetPlayerViewPoint(CamLoc, CamRot);
-
-    LogoWidget->SetWidgetClass(LogoWidgetClass);
-    LogoWidget->SetWorldLocation(CamLoc + CamRot.Vector() * 150.0f);
-    LogoWidget->SetWorldRotation(CamRot);
-
-    ShowTitleLogo();
-
     FTimerHandle FadeInHandle;
     GetWorld()->GetTimerManager().SetTimer(
         FadeInHandle,
@@ -56,43 +30,6 @@ void AIntroPlayer::BeginPlay()
         2.0f,
         false
     );
-}
-
-void AIntroPlayer::Tick(float DeltaTime)
-{
-    Super::Tick(DeltaTime);
-
-    if (bLogoFading)
-    {
-        LogoFadeCurrent += DeltaTime;
-        float Alpha = FMath::Clamp(LogoFadeCurrent / LogoFadeTime, 0.0f, 1.0f);
-
-        float Opacity = 1.0f - Alpha;
-
-        if (UUserWidget* W = LogoWidget->GetUserWidgetObject())
-        {
-            W->SetRenderOpacity(Opacity);
-        }
-
-        if (Alpha >= 1.0f)
-        {
-            bLogoFading = false;
-            LogoWidget->SetVisibility(false);
-        }
-    }
-
-
-    FVector CamLoc;
-    FRotator CamRot;
-    UGameplayStatics::GetPlayerController(GetWorld(), 0)->GetPlayerViewPoint(CamLoc, CamRot);
-
-    LogoWidget->SetWorldLocation(CamLoc + CamRot.Vector() * 150.0f);
-    LogoWidget->SetWorldRotation(CamRot);
-}
-
-void AIntroPlayer::ShowTitleLogo()
-{
-    // Widget ‚Í BeginPlay ‚ÅƒZƒbƒgÏ‚Ý‚È‚Ì‚Å‰½‚à‚µ‚È‚¢
 }
 
 void AIntroPlayer::FadeInFromBlack()
@@ -106,19 +43,4 @@ void AIntroPlayer::FadeInFromBlack()
         false,
         false
     );
-
-    FTimerHandle LogoFadeHandle;
-    GetWorld()->GetTimerManager().SetTimer(
-        LogoFadeHandle,
-        this,
-        &AIntroPlayer::StartLogoFadeOut,
-        1.0f,
-        false
-    );
-}
-
-void AIntroPlayer::StartLogoFadeOut()
-{
-    bLogoFading = true;
-    LogoFadeCurrent = 0.0f;
 }
